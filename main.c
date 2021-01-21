1e5ba804c7f37f74d2be4fbeef41e2eb29922aa8
Ben Ferguson <ferguson.ben86@gmail.com>
2021-01-21 00:23:11 -0500

    progress

--- main.c
+++ main.c
@@ -5,19 +5,18 @@
 
 #include "pc88-c.h"
 
-
 // Consts
 String strdat = "H e l l o   W o r l d \x00";
 
 void main()
 {
     SCREEN_POINTER = (vu8*)SCREEN_TXT_BASE;
-    
+    /*
     // turn on 80 col + color mode 
-    SetIOReg(SYS_CTL_REGISTER, (u8)TEXT_80COL_BIT); 
-    
+    SetIOReg(SYS_CTL_REGA, TEXT_BWMODE_BIT | TEXT_80COL_BIT); 
     // Print tests
     ClearAttributeRam();
+    //SetIOReg(0x31, )
     SetTextAttribute(0, 0, COLORMODE_SET(CLR_FUSCHIA, true));
     SetTextAttribute(20, 0, COLORMODE_ATTR(false, false, false, true, false));
     SetTextAttribute(5, 1, COLORMODE_SET(CLR_WHITE, false));
@@ -25,14 +24,38 @@ void main()
     print(strdat);
     SetCursorPos(5, 1);
     print("Hello World Too\x00");
-    
+    */
     // Set ALU mode on!
-    u8 t = ReadIOReg(MODE_SET_REGISTER);
-    SetIOReg(MODE_SET_REGISTER, t ^ bit(6));
-    // reset alu ctl reg 
-    SetIOReg(EXPANDED_ALU_CTRL, 0xff);
-    SetIOReg(EXPANDED_GVRAM_CTRL, 0x80);
+    //u8 t = ReadIOReg(MODE_SET_REGISTER);
+    //SetIOReg(MODE_SET_REGISTER, t | bit(6));
+    //SetIOReg(EXPANDED_ALU_CTRL, 0xff);
+    //SetBGColor(CLR_RED);
     
+    // 25 line, color, gfc on, n88, 64kb, 200line
+    SetIOReg(0x31, 0b00111011);
+    // GVRAM access on (enable multiplexer)
+    SetIOReg(0x35, (u8)bit(7));
+    // Hide text 
+    SetIOReg(0x53, 1);
+    // pixels
+    SETBANK_BLUE();
+    vu8* p = (u8*)0xc000;
+    *p = 0xff;
+    SETBANK_RED();
+    *p = 1;
+    p++;
+    *p = 1;
+
+
+    SetIOReg(0x32, bit(6)); // EXPANDED on
+    SetIOReg(0x35, (u8)(bit(7)|bit(4))); // ALU on, copy mode
+    p++;
+    *p = *(p-1); // copy one byte over
+    
+    // Reenable main ram access
+    SETBANK_MAINRAM();
+    SetIOReg(EXPANDED_GVRAM_CTRL, 0);
+
     while(1)
     {
         Wait_VBLANK();
--- pc88-c.h
+++ pc88-c.h
@@ -14,7 +14,8 @@ typedef unsigned char bool;
 #define SCREEN_TXT_BASE 0xf3c8
 #define SCREEN_ATTR_BASE SCREEN_TXT_BASE + 80
 
-#define SYS_CTL_REGISTER (u8)0x30
+#define SYS_CTL_REGA (u8)0x30
+#define SYS_CTL_REGB (u8)0x31
 #define MODE_SET_REGISTER (u8)0x32
 #define EXPANDED_ALU_CTRL (u8)0x34
 #define EXPANDED_GVRAM_CTRL (u8)0x35
@@ -37,6 +38,15 @@ typedef unsigned char bool;
 #define CLR_BLUE 1
 #define CLR_BLACK 0
 
+#define PAL_REG0 0x54
+#define PAL_REG1 0x55
+#define PAL_REG2 0x56
+#define PAL_REG3 0x57
+#define PAL_REG4 0x58
+#define PAL_REG5 0x59
+#define PAL_REG6 0x5a
+#define PAL_REG7 0x5b
+
 #define COLORMODE_SET(color, semigraphic) (u8)((color<<5)|(semigraphic<<4)|0b1000)
 #define COLORMODE_ATTR(underline, upperline, reverse, blink, hidden) (u8)((underline<<5)|(upperline<<4)|(reverse<<2)|(blink<<1)|(hidden))
 #define BWMODE_ATTR(underline, upperline, reverse, blink, hidden) (u8)((underline<<5)|(upperline<<4)|(reverse<<2)|(blink<<1)|(hidden))
@@ -55,7 +65,11 @@ void ClearAttributeRam();
 void SetCursorPos(u8 x, u8 y);
 /**/void SetCursorPos40(u8 x, u8 y);
 void Wait_VBLANK();
-
-
+#define SetBGColor(c) SetIOReg(0x52, c << 4)
+#define SetBorderColor(c) SetIOReg(0x52, c)
+#define SETBANK_BLUE() SetIOReg(0x5c, 1)
+#define SETBANK_RED() SetIOReg(0x5d, 1)
+#define SETBANK_GREEN() SetIOReg(0x5e, 1)
+#define SETBANK_MAINRAM() SetIOReg(0x5f, 1)
 // globals 
 vu8* SCREEN_POINTER;
