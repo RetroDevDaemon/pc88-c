a681075330dd0f6cea91d8b89d9be8f3f0cb08ba
Ben Ferguson <ferguson.ben86@gmail.com>
2021-02-04 18:51:57 -0500

    Fix for init code

--- src/main.c
+++ src/main.c
@@ -16,8 +16,6 @@ PlanarBitmap layeredImage = {
     img_r, img_g, img_b, 248/8, 100
 };
 
-u8 LINE_POINTER;
-
 /// Keep me here!
 void main()
 {   
@@ -28,10 +26,6 @@ void main()
     //SetIOReg(0x31, 0b00111011);
     //SetIOReg(0x32, 0b10011001); // default state
     //SetIOReg(0x35, (u8)bit(7)); // GVRAM access on 
-    
-    // Required to initialize print/putchr
-    SCREEN_POINTER = (vu8*)SCREEN_TXT_BASE;
-    LINE_POINTER = (u8)0;
 
     IRQ_OFF
     // Test yellow ALU draw (V2)
@@ -40,11 +34,13 @@ void main()
     SetIOReg(EXPANDED_ALU_CTRL, CLR_YELLOW);     // OR bits or bit reset to set color
     vu8* vp = (vu8*)0xc100;
     *vp = 0xff;
-    ExpandedGVRAM_Off();
-    // Toggle, then test blue ALU draw (v2)
-    ExpandedGVRAM_On();
+    SetIOReg(EXPANDED_GVRAM_CTRL, (u8)(0x80 | CLR_WHITE | bit(4)) );
     SetIOReg(EXPANDED_ALU_CTRL, CLR_BLUE);
-    vp += 0x100;
+    __asm
+      ld a,(0xC100)
+      ld (0xC101),a
+    __endasm;
+    SetIOReg(EXPANDED_GVRAM_CTRL, (u8)(0x80 | CLR_WHITE ) );
     *vp = 0xff;
     ExpandedGVRAM_Off();
     // Planar bitmap (V1) draw and individual pixels
@@ -66,183 +62,6 @@ void main()
     }
 }
 
-
-bool GetKeyDown(u8 SCANCODE)
-{
-    u8 r = (SCANCODE & 0xf0) >> 4;
-    u8 b = SCANCODE & 0x0f;
-    u8 i = ReadIOReg(r);
-    if(! (i & bit(b)) ) return true;
-    return false;
-}
-
-void Wait_VBLANK() __naked 
-{
-    /* Taken from PC-88 info page:
-     http://mydocuments.g2.xrea.com/html/p8/vraminfo.html */
-
-    __asm
-    WaitVBlank:
-    in	    a,(0x40)        ; SYS_MODE_SENSE
-    and     #0x20           ; VBLANK_SIGNAL
-    jr      nz,WaitVBlank   ; bit 5 set is vblank interval 
-    .wait:
-    in      a,(0x40)
-    and     #0x20
-    jr      z,.wait         ; wait until the signal is off again to be safe!
-    ret
-    __endasm;
-}
-
-void SetCursorPos(u8 x, u8 y)
-{
-    SCREEN_POINTER = (vu8*)(SCREEN_TXT_BASE + (120 * y) + x);
-}
-
-void SetCursorPos40(u8 x, u8 y)
-{
-    SCREEN_POINTER = (vu8*)(SCREEN_TXT_BASE + (120 * y) + (x * 2));
-}
-
-void ClearAttributeRam()
-{
-    vu8* addr = (vu8*)(SCREEN_ATTR_BASE);
-    u16 p = 0xe880;
-    for(u8 y = 0; y < 24; y++){
-        for(u8 x = 0; x < 20; x++){
-            *addr = p;
-            addr += 2;   
-        }
-        addr += 80;
-    }
-}
-
-void SetTextAttribute(u8 x, u8 y, u8 attr)
-{
-    vu8* addr = (vu8*)(SCREEN_ATTR_BASE + (y * 120));
-    while(*addr != 0x80)
-        addr += 2;
-
-    *addr = x; 
-    addr++;
-    *addr = attr;
-}
-
-static inline void putchr(u8 c)
-{
-    *SCREEN_POINTER = c;
-    SCREEN_POINTER++;
-    LINE_POINTER++;
-    if(LINE_POINTER == 80) { 
-        LINE_POINTER = 0;
-        SCREEN_POINTER = SCREEN_POINTER + 40;
-    }
-}
-
-static inline void putchr40(u8 c)
-{
-    *SCREEN_POINTER = c;
-    SCREEN_POINTER +=2 ;
-    LINE_POINTER++;
-    if(LINE_POINTER == 80) { 
-        LINE_POINTER = 0;
-        SCREEN_POINTER = SCREEN_POINTER + 40;
-    }
-}
-
-void print(String str)
-{
-    u8 i = 0;
-    while(str[i] != '\x00')
-        putchr(str[i++]);
-}
-
-void print40(String str)
-{
-    u8 i = 0;
-    while(str[i] != '\x00')
-        putchr40(str[i++]);
-}
-
-
-u8 ReadIOReg(u8 r) __naked
-{   // Template copied from generated SDCC code and "optimized"
-    r;
-    __asm 
-        ld	iy, #2
-        add	iy, sp
-        ld a, 0 (iy)
-        ld c, a 
-        in a, (c)
-        ld 0 (iy), a 
-        ld	l, 0 (iy)
-        ret 
-    __endasm;
-}
-
-void SetIOReg(u8 r, u8 v) __naked 
-{   
-    r; v; 
-    __asm
-        push ix 
-        ld ix,#0
-        add	ix,sp
-    //; 4 (ix) and 5 (ix) contain r and v 
-        ld c, 4 (ix)
-        ld a, 5 (ix) 
-        out (c), a 
-        ld l, a
-        pop	ix
-        ret
-    __endasm;
-}
-
-
-void DrawPlaneBMP(const u8* img, u8 plane, u16 x, u16 y, u8 w, u8 h)
-{
-    if(plane == PLANE_RED)
-        SETBANK_RED()
-    else if(plane == PLANE_BLUE)
-        SETBANK_BLUE()
-    else 
-        SETBANK_GREEN()
-    vu8* p = (vu8*)(0xc000) + (y * 80) + (u8)(x/8);
-    const u8* v = img;
-    for(u8 y = 0; y < h; y++){
-        for(u8 x = 0; x < w; x++){
-            *p = *v;
-            p++;
-            v++;
-        }
-        p += (80 - w);
-    } 
-}
-
-
-void SetPixel(u16 x, u8 y, u8 c)
-{
-    u8 po = 7 - ((x & 0xff) % 8);
-    vu8* p = (vu8*)(0xc000) + (y * 80) + (u8)(x / 8);
-        
-    if(c & 0b001){
-        SETBANK_BLUE()
-        *p |= bit(po);
-    }
-    if(c & 0b010){
-        SETBANK_RED()
-        *p |= bit(po);
-    }
-    if(c & 0b100){
-        SETBANK_GREEN()
-        *p |= bit(po);
-    }
-
-}
-
-static inline void EnableALU(){ SetIOReg(ALU_MODE_CTRL, 0xC9); }
-static inline void DisableALU(){ SetIOReg(ALU_MODE_CTRL, 0x89); }
-static inline void ExpandedGVRAM_On() { SetIOReg(EXPANDED_GVRAM_CTRL, 0x80); }
-static inline void ExpandedGVRAM_Off() { SetIOReg(EXPANDED_GVRAM_CTRL, 0); } 
 
 
 /* Pal test for layered planar gfx 
--- src/pc88-c.h
+++ src/pc88-c.h
@@ -268,7 +268,7 @@ GV2 - ALU mode for GVRAM bank 2
 10: Bit invert
 11: No op */
 #define EXPANDED_ALU_CTRL 0x34 
-/* Expanded Access Mode GVRAM Control
+/* Expanded Access Mode GVRAM Control   W only
 bit  7      6   5 4     3   2   1   0
      GAM    -   GDM     -   GC  RC  BC
 
@@ -302,9 +302,8 @@ not stored in a register. What is loaded in the register is
 ld a,%10010000  ; gvram access on, 3 plane buffer r/w mode
 out ($35),a 
 ld a,($c000)	; this is bit comp data, but the
-ld ($c0002),a   ; operation commands a vram 'copy'.
-*/
-#define EXPANDED_GVRAM_CTRL 0x35  // W only
+ld ($c0002),a   ; operation commands a vram 'copy'.*/
+#define EXPANDED_GVRAM_CTRL 0x35
 // Bit 6 - Joystick port output
 #define SYS_MODE_SENSE 0x40   // Write
 #define STROBE_PORT 0x40      // Read
@@ -416,6 +415,7 @@ Writing 0xff selects Main ROM.
 
 
 // Function headers
+void __init();
 void main();
 static inline void putchr(u8 c);
 /**/static inline void putchr40(u8 c);
@@ -470,4 +470,189 @@ static inline void ExpandedGVRAM_Off();
 
 // globals 
 vu8* SCREEN_POINTER;
+u8 LINE_POINTER;
 
+void __init()
+{
+    // Required to initialize print/putchr
+    SCREEN_POINTER = (vu8*)SCREEN_TXT_BASE;
+    LINE_POINTER = (u8)0;
+    main();
+}
+
+bool GetKeyDown(u8 SCANCODE)
+{
+    u8 r = (SCANCODE & 0xf0) >> 4;
+    u8 b = SCANCODE & 0x0f;
+    u8 i = ReadIOReg(r);
+    if(! (i & bit(b)) ) return true;
+    return false;
+}
+
+void Wait_VBLANK() __naked 
+{
+    /* Taken from PC-88 info page:
+     http://mydocuments.g2.xrea.com/html/p8/vraminfo.html */
+
+    __asm
+    WaitVBlank:
+    in	    a,(0x40)        ; SYS_MODE_SENSE
+    and     #0x20           ; VBLANK_SIGNAL
+    jr      nz,WaitVBlank   ; bit 5 set is vblank interval 
+    .wait:
+    in      a,(0x40)
+    and     #0x20
+    jr      z,.wait         ; wait until the signal is off again to be safe!
+    ret
+    __endasm;
+}
+
+void SetCursorPos(u8 x, u8 y)
+{
+    SCREEN_POINTER = (vu8*)(SCREEN_TXT_BASE + (120 * y) + x);
+}
+
+void SetCursorPos40(u8 x, u8 y)
+{
+    SCREEN_POINTER = (vu8*)(SCREEN_TXT_BASE + (120 * y) + (x * 2));
+}
+
+void ClearAttributeRam()
+{
+    vu8* addr = (vu8*)(SCREEN_ATTR_BASE);
+    u16 p = 0xe880;
+    for(u8 y = 0; y < 24; y++){
+        for(u8 x = 0; x < 20; x++){
+            *addr = p;
+            addr += 2;   
+        }
+        addr += 80;
+    }
+}
+
+void SetTextAttribute(u8 x, u8 y, u8 attr)
+{
+    vu8* addr = (vu8*)(SCREEN_ATTR_BASE + (y * 120));
+    while(*addr != 0x80)
+        addr += 2;
+
+    *addr = x; 
+    addr++;
+    *addr = attr;
+}
+
+static inline void putchr(u8 c)
+{
+    *SCREEN_POINTER = c;
+    SCREEN_POINTER++;
+    LINE_POINTER++;
+    if(LINE_POINTER == 80) { 
+        LINE_POINTER = 0;
+        SCREEN_POINTER = SCREEN_POINTER + 40;
+    }
+}
+
+static inline void putchr40(u8 c)
+{
+    *SCREEN_POINTER = c;
+    SCREEN_POINTER +=2 ;
+    LINE_POINTER++;
+    if(LINE_POINTER == 80) { 
+        LINE_POINTER = 0;
+        SCREEN_POINTER = SCREEN_POINTER + 40;
+    }
+}
+
+void print(String str)
+{
+    u8 i = 0;
+    while(str[i] != '\x00')
+        putchr(str[i++]);
+}
+
+void print40(String str)
+{
+    u8 i = 0;
+    while(str[i] != '\x00')
+        putchr40(str[i++]);
+}
+
+
+u8 ReadIOReg(u8 r) __naked
+{   // Template copied from generated SDCC code and "optimized"
+    r;
+    __asm 
+        ld	iy, #2
+        add	iy, sp
+        ld a, 0 (iy)
+        ld c, a 
+        in a, (c)
+        ld 0 (iy), a 
+        ld	l, 0 (iy)
+        ret 
+    __endasm;
+}
+
+void SetIOReg(u8 r, u8 v) __naked 
+{   
+    r; v; 
+    __asm
+        push ix 
+        ld ix,#0
+        add	ix,sp
+    //; 4 (ix) and 5 (ix) contain r and v 
+        ld c, 4 (ix)
+        ld a, 5 (ix) 
+        out (c), a 
+        ld l, a
+        pop	ix
+        ret
+    __endasm;
+}
+
+
+void DrawPlaneBMP(const u8* img, u8 plane, u16 x, u16 y, u8 w, u8 h)
+{
+    if(plane == PLANE_RED)
+        SETBANK_RED()
+    else if(plane == PLANE_BLUE)
+        SETBANK_BLUE()
+    else 
+        SETBANK_GREEN()
+    vu8* p = (vu8*)(0xc000) + (y * 80) + (u8)(x/8);
+    const u8* v = img;
+    for(u8 y = 0; y < h; y++){
+        for(u8 x = 0; x < w; x++){
+            *p = *v;
+            p++;
+            v++;
+        }
+        p += (80 - w);
+    } 
+}
+
+
+void SetPixel(u16 x, u8 y, u8 c)
+{
+    u8 po = 7 - ((x & 0xff) % 8);
+    vu8* p = (vu8*)(0xc000) + (y * 80) + (u8)(x / 8);
+        
+    if(c & 0b001){
+        SETBANK_BLUE()
+        *p |= bit(po);
+    }
+    if(c & 0b010){
+        SETBANK_RED()
+        *p |= bit(po);
+    }
+    if(c & 0b100){
+        SETBANK_GREEN()
+        *p |= bit(po);
+    }
+
+}
+
+static inline void EnableALU(){ SetIOReg(ALU_MODE_CTRL, 0xC9); }
+static inline void DisableALU(){ SetIOReg(ALU_MODE_CTRL, 0x89); }
+static inline void ExpandedGVRAM_On() { SetIOReg(EXPANDED_GVRAM_CTRL, 0x80); }
+static inline void ExpandedGVRAM_Off() { SetIOReg(EXPANDED_GVRAM_CTRL, 0); } 
