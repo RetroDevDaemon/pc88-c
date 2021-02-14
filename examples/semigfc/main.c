fae853f1aa2466462517ebf4ee43519cfbf4d439
Ben Ferguson <ferguson.ben86@gmail.com>
2021-02-14 01:04:49 -0500

    semigraphic example

--- examples/semigfc/main.c
+++ examples/semigfc/main.c
@@ -12,24 +12,45 @@ void main()
     // -First attribute on each row is always counted as X=0
     // -Remember to enable/disable as needed
     
+    // Byte layout:
+    // 0044  where:
+    // 1155   76543210 are bits in the byte
+    // 2266    and R is if reverse is enabled
+    // 3377
+    // RRRR
+     // One byte makes 20 pixels on screen.
+
+    // E.g. python3 tools/png2zemi.py semi-rose.png
+    /* outputs:
+    [['0x1f', '0x31', '0x3f', '0xf1'], '0x78', 255, 
+    ['0x70', '0xef', '0xf9', '0x7'], '0x58', 0, 
+    ['0xb8', '0x7', '0xf0', '0xb'], '0x98', 255, 
+    ['0xde', '0x8d', '0xd0', '0xfe'], '0xd8', 255]
+        Character data, COLORMODE_SET, and if you need reverse on (255 = true)
+    */
     // Rose:
-    print("\xe0\xce\xc0\x0e\x00");
+    print("\x80\x1f\x31\x3f\xf1\x00");
     SetCursorPos(0, 1);
-    print("\x70\xef\xf9\x07\x00");
-    SetCursorPos(0, 2);
-    print("\x47\xf8\x8f\xf4\x00");
+    print("\xff\x70\xef\xf9\x07\x00");
+    SetCursorPos(1, 2);
+    print("\xb8\x07\xf0\x0b\x00");
     SetCursorPos(0, 3);
-    print("\x10\x72\x3f\x01\x00");
+    print("\xff\xde\x8d\xd0\xfe\x00");
     SetTextAttribute(0, 0, COLORMODE_SET(CLR_FUSCHIA, true));  // color + semi 
     SetTextAttribute(1, 0, COLORMODE_ATTR(0, 0, true, 0, 0));// reverse
-    SetTextAttribute(4, 0, COLORMODE_SET(CLR_BLACK, true));
+    SetTextAttribute(5, 0, COLORMODE_SET(CLR_BLACK, true));
+    
     SetTextAttribute(0, 1, 0x58);
-    SetTextAttribute(4, 1, COLORMODE_SET(CLR_BLACK, true));
+    SetTextAttribute(1, 1, 0);// reverse off
+    SetTextAttribute(5, 1, COLORMODE_SET(CLR_BLACK, true));
+    
     SetTextAttribute(0, 2, 0x98);
-    SetTextAttribute(4, 2, COLORMODE_SET(CLR_BLACK, true));
+    SetTextAttribute(1, 2, COLORMODE_ATTR(0, 0, true, 0, 0));// reverse on
+    SetTextAttribute(5, 2, COLORMODE_SET(CLR_BLACK, true));
+    
     SetTextAttribute(0, 3, 0xd8);
-    SetTextAttribute(4, 3, COLORMODE_SET(CLR_BLACK, true));
-    SetTextAttribute(5, 3, 0);// reverse off 
+    SetTextAttribute(5, 3, COLORMODE_SET(CLR_BLACK, true));
+    SetTextAttribute(6, 3, 0);
     
     IRQ_ON
 
--- tools/png2zemi.py
+++ tools/png2zemi.py
@@ -16,10 +16,17 @@ b2 = numpy.asarray(img)
 b3 = Image.new('L', (b2.shape[1], b2.shape[0]), 0)
 o = []
 
+def invert(r):
+    i = 0
+    while i < len(r):
+        r[i] = hex(0xff ^ int(r[i],16))
+        i += 1
+    return r
+
 a = []
 wid = img.size[0]
 hei = img.size[1]
-for yt in range(0, hei, 4):
+for yt in range(0, hei, 5):
     row = []
     for xt in range(0, wid, 2):
         ob = ''
@@ -32,8 +39,16 @@ for yt in range(0, hei, 4):
                     ob = '1' + ob
                 else:
                     ob = '0' + ob 
+            
         row.append(hex(int(ob,2)))
-    a.append(row)
-    a.append(hex((c << 5) | (1 << 3) | (1 << 4)))
+    if(img.getpixel((0, yt+4)) != 0):
+        row = invert(row)
+        a.append(row)
+        a.append(hex((c << 5) | (1 << 3) | (1 << 4)))    
+        a.append(255)
+    else:
+        a.append(row)
+        a.append(hex((c << 5) | (1 << 3) | (1 << 4)))
+        a.append(0)
 print(a)
                     
