3e7f067d9163bec5d596d71c129e641fae5b3c30
bferguson3 <ferguson.ben86@gmail.com>
2021-05-10 14:25:36 -0400

    Merge progress from develop

--- examples/tilemap-wip/maps/tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tileset" tilewidth="32" tileheight="16" tilecount="2" columns="2">
- <image source="tileset.png" width="64" height="16"/>
-</tileset>
