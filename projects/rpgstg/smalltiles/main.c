d9133ba39f03231c799ddbd4fd7dde6646fa64a4
Ben Ferguson <ferguson.ben86@gmail.com>
2022-08-31 18:06:29 -0400

    tweaks

--- projects/rpgstg/smalltiles/main.c
+++ projects/rpgstg/smalltiles/main.c
@@ -3,6 +3,10 @@
 
 #include "tileset.h"
 
+void CopyTileToBuffer(u8* src, u8* dst);
+void DrawTileFromBuffer(u8* src, u8* dst);
+
+
 void main()
 {
     IRQ_OFF;
@@ -13,67 +17,59 @@ void main()
     // load tiles into vram
     for(u8 i = 0; i < 24; i++)
         DrawImage_V2(0 + (i * 2), 0, (&tileset[0] + (64 * i)), 2, 8);
+    // tile is stored in 4bpp format on disk, which is 64 bytes per tile
+    //   when loaded in vram, it is only 16 bytes with alu
     
     // out
     ExpandedGVRAM_Copy_On();
     
     // copy out tiles 
-    __asm 
-        ld hl,#0xc002
-        ld de,#0xfe80
-        ld bc,#8
-
-        ._tilealus:
-        ld a,(hl)
-        ld (de),a 
-        inc hl 
-        inc de
-        ld a,(hl)
-        ld (de),a 
-        inc hl 
-        inc de
-
-        push bc 
-        ld bc,#78
-        add hl,bc
-        pop bc 
-        djnz ._tilealus
-
-    __endasm;
-
-    __asm 
-        ld hl,#0xfe80
-        ld de,#0xd000
-        ld bc,#8
-
-        ._tilealus2:
-        ld a,(hl)
-        ld (de),a 
-        inc hl 
-        inc de
-        ld a,(hl)
-        ld (de),a 
-        inc hl 
-        inc de
-
-        push bc
-        push hl 
-        push de 
-        pop hl 
-        ld bc,#78
-        add hl,bc
-        push hl 
-        pop de
-        pop hl  
-        pop bc 
-        djnz ._tilealus2
-
-    __endasm;
-
+    CopyTileToBuffer(0xc002, 0xfe80);
+    
+    DrawTileFromBuffer(0xfe80, 0xc200);
+    
     DisableALU(FASTMEM_OFF);
     ExpandedGVRAM_Off();
 
     IRQ_ON;
     while(1){};
 
+}
+
+void CopyTileToBuffer(u8* src, u8* dst) 
+{
+    // HL and DE are already loaded
+    __asm 
+        ld bc,#8
+        _tcpoutlp:
+        push bc 
+        ldi
+        ldi 
+        ld bc,#78
+        add hl,bc
+        pop bc 
+        djnz _tcpoutlp
+    __endasm;
+}
+
+void DrawTileFromBuffer(u8* src, u8* dst) 
+{
+    // HL and DE are already loaded
+    __asm 
+        ld bc,#8
+        _tcpinlp:
+        push bc
+        push hl         ; faster way?
+        push de 
+        pop hl 
+        ld bc,#78
+        add hl,bc 
+        push hl 
+        pop de 
+        pop hl 
+        ldi
+        ldi 
+        pop bc 
+        djnz _tcpinlp
+    __endasm;
 }
